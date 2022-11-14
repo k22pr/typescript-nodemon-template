322046a3ad633a196e501f4d30c99c75afbdc59f
k22pr <h2x@pm.me>
2022-11-14 19:35:36 +0800

    fix : add redis service

---
+++ src/services/redis.ts
@@ -0,0 +1,12 @@
+// import * as redis from "redis";
+
+// const connectUrl = `rediss://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+// const client = redis.createClient({
+//   url: connectUrl,
+//   username: process.env.REDIS_USERNAME,
+//   password: process.env.REDIS_PASSWORD,
+// });
+
+// client.on("error", (err) => console.log("Redis Client Error", err));
+
+// export default client;
